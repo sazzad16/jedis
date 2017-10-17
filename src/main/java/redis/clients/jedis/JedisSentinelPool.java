b5f339171bc1956c7f16bd6389cc04ae21ed6696
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2017-10-17 17:33:40 +0600

    Added Jedis(HostAndPort) and similar constructors (#14)
    This helps to replace all the usages of 'new Jedis(HostAndPort.getHost(), HostAndPort.getPort())' and similar object creations in shorter and safer manner.

--- src/main/java/redis/clients/jedis/JedisSentinelPool.java
+++ src/main/java/redis/clients/jedis/JedisSentinelPool.java
@@ -142,7 +142,7 @@ public class JedisSentinelPool extends Pool<Jedis> {
 
       Jedis jedis = null;
       try {
-        jedis = new Jedis(hap.getHost(), hap.getPort());
+        jedis = new Jedis(hap);
 
         List<String> masterAddr = jedis.sentinelGetMasterAddrByName(masterName);
 
