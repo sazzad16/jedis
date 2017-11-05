cca768cb9bd31c66ca591493b43206b38656bdab
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2017-11-05 12:26:51 +0600

    Added 'pttl' command in remaining binary interfaces and classes (#56)

--- src/main/java/redis/clients/jedis/BinaryShardedJedis.java
+++ src/main/java/redis/clients/jedis/BinaryShardedJedis.java
@@ -113,6 +113,11 @@ public class BinaryShardedJedis extends Sharded<Jedis, JedisShardInfo> implement
     return j.ttl(key);
   }
 
+  public Long pttl(byte[] key) {
+    Jedis j = getShard(key);
+    return j.pttl(key);
+  }
+
   public Long touch(byte[] key) {
     Jedis j = getShard(key);
     return j.touch(key);
