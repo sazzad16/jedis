137a776d64acbe6c8b2bc8c7459503785ec5e1b0
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2017-11-05 12:26:51 +0600

    Added 'pttl' command in remaining binary interfaces and classes (#56)

--- src/main/java/redis/clients/jedis/BinaryJedis.java
+++ src/main/java/redis/clients/jedis/BinaryJedis.java
@@ -3279,6 +3279,7 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands, MultiKey
     return client.getIntegerReply();
   }
 
+  @Override
   public Long pttl(final byte[] key) {
     checkIsInMultiOrPipeline();
     client.pttl(key);
--- src/main/java/redis/clients/jedis/BinaryShardedJedis.java
+++ src/main/java/redis/clients/jedis/BinaryShardedJedis.java
@@ -113,6 +113,11 @@ public class BinaryShardedJedis extends Sharded<Jedis, JedisShardInfo> implement
     return j.ttl(key);
   }
 
+  public Long pttl(byte[] key) {
+    Jedis j = getShard(key);
+    return j.pttl(key);
+  }
+
   public byte[] getSet(byte[] key, byte[] value) {
     Jedis j = getShard(key);
     return j.getSet(key, value);
